4e3057bb65e5a020104d0304228581051ae18be8
DiogoNMs <97695585+DiogoNMs@users.noreply.github.com>
2024-04-05 12:36:19 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let number = getRandomInt;
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  console.log(getRandomInt(1, 6)); // Gera um número aleatório entre 1 e 6
+
+if number / 2  % 0: 
+
+/*while (continuar) {
+    let opcao = prompt("Escolha uma opção:\n1. Par\n2. Impar);
+
+    switch (opcao) {
+        case '1':
+       
+        default:
+            alert("Opção inválida. Por favor, escolha uma opção de 1 e 2.");
+*/
